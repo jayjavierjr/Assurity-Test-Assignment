e2ffa0cfa600a0faf8d95b75de3cfac43a27522d
JayJavierJr <jay.javier@dia.govt.nz>
2026-05-08 20:16:04 +1200

    Initial commit - Assurity performance test assignment

---
+++ LoadTestRunner.bat
@@ -0,0 +1,23 @@
+@echo off
+
+cd C:\apache-jmeter-5.6.3\bin
+
+set BASEDIR=C:\Temp\TestAssignment
+
+rmdir /s /q "%BASEDIR%\results\HTMLReport"
+
+del /q "%BASEDIR%\results\results.jtl"
+
+del /q "%BASEDIR%\results\jmeter.log"
+
+jmeter -n ^
+-t "%BASEDIR%\JayJavier_AssurityTestAssignment.jmx" ^
+-l "%BASEDIR%\results\results.jtl" ^
+-j "%BASEDIR%\results\jmeter.log" ^
+-e -o "%BASEDIR%\results\HTMLReport" ^
+-Jthreads=5 ^
+-Jrampup=5 ^
+-Jduration=65 ^
+-Jthroughput=10
+
+pause
